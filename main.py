2a28df66da92c1b4ac261c97c37e10658e1bcbc1
Nan <naen.mong@gmail.com>
2019-03-27 21:26:25 +0800

    modify the data.py

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+import cv2
+import os, sys
+import glob
+import random
+
+from model.data import *
+
+data = Data("data/Lat1-100")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(0)
