fec98adfff24fc2f4daacb840ca279d90e4ff4b9
Nan Meng <naen.mong@gmail.com>
2019-05-24 22:14:50 +0800

    Update download.sh

--- data/download.sh
+++ data/download.sh
@@ -1,2 +1,9 @@
-wget https://github.com/monaen/MedicalAnalysis/raw/data/Lat1-100.zip
-wget
+echo "Downloading......"
+
+wget --no-check-certificate https://github.com/monaen/MedicalAnalysis/raw/data/Lat0001-1000_processed.zip
+
+echo "Unzipping......"
+
+unzip x Lat0001-1000_processed.zip && rm -f Lat0001-1000_processed.zip
+
+echo "Done."
